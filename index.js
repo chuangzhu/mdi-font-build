96211ca6595aa3e129b07349b96a8d15a99b996a
Chuang Zhu <genelocated@yandex.com>
2020-10-22 21:35:31 +0800

    remove unused code

--- index.js
+++ index.js
@@ -4,39 +4,35 @@
 const webfont = require("webfont").default;
 const sass = require('sass');
 const fs = require('fs');
+const os = require('os')
 const path = require('path');
 const crypto = require('crypto')
 
 const mdiSvgPath = path.dirname(require.resolve('@mdi/svg/package.json'));
 const distFolder = './mdi';
+  const tmpdir = path.join(os.tmpdir(), 'mdi-font-build');
 
 function generateFolders() {
   if (!fs.existsSync(distFolder)) {
     fs.mkdirSync(distFolder, { recursive: true });
   }
-  const folders = ['scss', 'css', 'fonts'];
-  folders.forEach(folder => {
-    if (!fs.existsSync(path.join(distFolder, folder))) {
-      fs.mkdirSync(path.join(distFolder, folder));
-    }
-  });
-  console.log('- Folders created.');
+  if (!fs.existsSync(tmpdir)) {
+    fs.mkdirSync(tmpdir, { recursive: true });
+  }
 }
 
 function generateSCSS(config, icons) {
   const {
     fileName,
     fontName,
-    fontFamily,
     fontWeight,
     version,
   } = config;
   // {fileName}.scss
-  const main = path.resolve(__dirname, '..', 'src', 'scss', 'main.scss');
-  const mainDist = path.resolve(distFolder, 'scss', `${fileName}.scss`);
+  const main = path.resolve(__dirname, 'scss', 'main.scss');
+  const mainDist = path.resolve(tmpdir, `${fileName}.scss`);
   const mainString = fs.readFileSync(main, 'utf8');
   fs.writeFileSync(mainDist, mainString);
-  console.log(`- Generated ${fileName}.scss`);
   // Others
   const others = [
     'animated',
@@ -48,19 +44,19 @@ function generateSCSS(config, icons) {
     'variables'
   ];
   others.forEach(file => {
-    const other = path.resolve(__dirname, '..', 'src', 'scss', `_${file}.scss`);
-    const otherDist = path.resolve(distFolder, 'scss', `_${file}.scss`);
+    const other = path.resolve(__dirname, 'scss', `_${file}.scss`);
+    const otherDist = path.resolve(tmpdir, `_${file}.scss`);
     let otherString = fs.readFileSync(other, 'utf8')
       .replace(/prefix/g, 'mdi')
+      .replace(/mdi-css-mdi/g, 'mdi-css-prefix')
       .replace(/fileName/g, fileName)
       .replace(/fontFamily/g, fileName)
       .replace(/fontName/g, fontName)
       .replace(/fontWeight/g, fontWeight)
-      .replace(/-.-.-/g, version)
-      .replace(/mdi-css-mdi/g, 'mdi-css-prefix');
+      .replace(/-.-.-/g, version);
     if (file === 'variables') {
       const iconMappings = icons.map((icon, i) => (
-        `  "${icon}": F${(i+'').padStart(4, '0')}`
+        `  "${icon}": F${(i + '').padStart(4, '0')}`
       ))
       otherString = otherString.replace(/icons: \(\)/, `icons: (\n${iconMappings.join(',\n')}\n)`);
     }
@@ -71,25 +67,25 @@ function generateSCSS(config, icons) {
 function generateCSS(config) {
   const { fileName } = config;
   sass.render({
-    file: path.resolve(distFolder, 'scss', `${fileName}.scss`),
+    file: path.resolve(tmpdir, `${fileName}.scss`),
     outputStyle: 'expanded',
     outFile: `${fileName}.css`
   }, function (err, result) {
     if (err) {
       console.error(err);
     } else {
-      fs.writeFileSync(path.join(distFolder, 'css', `${fileName}.css`), result.css);
+      fs.writeFileSync(path.join(distFolder, `${fileName}.css`), result.css);
     }
   });
   sass.render({
-    file: path.resolve(distFolder, 'scss', `${fileName}.scss`),
+    file: path.resolve(tmpdir, `${fileName}.scss`),
     outputStyle: 'compressed',
     outFile: `${fileName}.css`
   }, function (err, result) {
     if (err) {
       console.error(err);
     } else {
-      fs.writeFileSync(path.join(distFolder, 'css', `${fileName}.min.css`), result.css);
+      fs.writeFileSync(path.join(distFolder, `${fileName}.min.css`), result.css);
     }
   });
 }
@@ -113,10 +109,10 @@ function genWebfont(icons) {
   })
     .then(result => {
       generateFolders();
-      fs.writeFileSync(path.join(distFolder, 'fonts', `materialdesignicons.ttf`), result.ttf);
-      fs.writeFileSync(path.join(distFolder, 'fonts', `materialdesignicons.eot`), result.eot);
-      fs.writeFileSync(path.join(distFolder, 'fonts', `materialdesignicons.woff`), result.woff);
-      fs.writeFileSync(path.join(distFolder, 'fonts', `materialdesignicons.woff2`), result.woff2);
+      fs.writeFileSync(path.join(distFolder, `materialdesignicons.ttf`), result.ttf);
+      fs.writeFileSync(path.join(distFolder, `materialdesignicons.eot`), result.eot);
+      fs.writeFileSync(path.join(distFolder, `materialdesignicons.woff`), result.woff);
+      fs.writeFileSync(path.join(distFolder, `materialdesignicons.woff2`), result.woff2);
       generateSCSS(result.config, icons);
       generateCSS(result.config);
       return result;
